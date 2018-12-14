dafe0cbaeeb5d1e1069598dec3135d7838a88fdf
Prajwal Bati <prajwalbati@gmail.com>
2018-12-14 12:35:21 +0545

    Added ember addson, listing of property

---
+++ app/helpers/rental-property-type.js
@@ -0,0 +1,13 @@
+import { helper } from '@ember/component/helper';
+
+const communityPropertyTypes = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+export function rentalPropertyType([propertyType]) {
+  return communityPropertyTypes.includes(propertyType)?'Community':'Standalone';
+}
+
+export default helper(rentalPropertyType);
